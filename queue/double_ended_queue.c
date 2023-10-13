df2c949b7430eeaba96aa9951bd0b9bd52e64134
Sahil Shefeek <sahilms345@gmail.com>
2023-10-13 23:59:00 +0530

    program++

---
+++ queue/double_ended_queue.c
@@ -0,0 +1,151 @@
+#include <stdio.h>
+#define MAX_SIZE 5
+
+typedef struct
+{
+    int queue[MAX_SIZE];
+    int front;
+    int rear;
+} Queue;
+
+void enqueue_front(Queue *q, int item)
+{
+    if (q->front == (q->rear + 1) % MAX_SIZE)
+        printf("Queue is full!\n");
+    else
+    {
+        if (q->front == -1)
+        {
+            q->front = 0;
+            q->rear = 0;
+        }
+        else
+            q->front = (q->front + MAX_SIZE - 1) % MAX_SIZE;
+        q->queue[q->front] = item;
+        printf("Added %d\n", item);
+    }
+}
+
+void enqueue_rear(Queue *q, int item)
+{
+    if (q->front == (q->rear + 1) % MAX_SIZE)
+        printf("Queue is full!\n");
+    else
+    {
+        if (q->front == -1)
+            q->front = 0;
+        q->rear = (q->rear + 1) % MAX_SIZE;
+        q->queue[q->rear] = item;
+    }
+}
+
+void dequeue_front(Queue *q)
+{
+    if (q->front == -1)
+        printf("Queue is empty!\n");
+    else
+    {
+        int del = q->queue[q->front];
+
+        if (q->front == q->rear)
+        {
+            q->front = -1;
+            q->rear = -1;
+        }
+        else
+            q->front = (q->front + 1) % MAX_SIZE;
+        printf("Deleted %d\n", del);
+    }
+}
+
+void dequeue_rear(Queue *q)
+{
+    if (q->front == -1)
+        printf("Queue is empty!\n");
+    else
+    {
+        int del = q->queue[q->rear];
+
+        if (q->front == q->rear)
+        {
+            q->front = -1;
+            q->rear = -1;
+        }
+        else
+            q->rear = (q->rear + MAX_SIZE - 1) % MAX_SIZE;
+        printf("Deleted %d\n", del);
+    }
+}
+
+void list(Queue *q)
+{
+    if (q->front == -1)
+    {
+        printf("Queue is empty!\n");
+        return;
+    }
+    printf("Front ==>  %d\n", q->front);
+    printf("Queue elements are :\n");
+    int i = q->front;
+    // for (int i = q->front; i <= q->rear; i = (i + 1) % MAX_SIZE)
+    while (1)
+    {
+        printf("%d \t", q->queue[i]);
+        if (i == q->rear)
+            break;
+        i = (i + 1) % MAX_SIZE;
+    }
+    printf("\n");
+    printf("Rear ==>  %d\n", q->rear);
+}
+
+int main(int argc, char *argv[])
+{
+    Queue q;
+    q.front = -1;
+    q.rear = -1;
+
+    int choice, item;
+    while (1)
+    {
+    get_choice:
+        printf("Select operation:\n"
+               "1. Enqueue via FRONT\n2. Dequeue via FRONT\n"
+               "3. Enqueue via REAR\n4. Dequeue via REAR\n"
+               "5. List\n6. Exit\n");
+        scanf("%d", &choice);
+        switch (choice)
+        {
+        case 1:
+            printf("Enter item to add:\n");
+            scanf("%d", &item);
+            enqueue_front(&q, item);
+            break;
+
+        case 2:
+            dequeue_front(&q);
+            break;
+        case 3:
+            printf("Enter item to add:\n");
+            scanf("%d", &item);
+            enqueue_rear(&q, item);
+            break;
+
+        case 4:
+            dequeue_rear(&q);
+            break;
+
+        case 5:
+            list(&q);
+            break;
+
+        case 6:
+            return 0;
+
+        default:
+            printf("Invalid operation!\nTry again.\n");
+            goto get_choice;
+        }
+    }
+    return 0;
+}
--- queue/polynomial_add.c
+++ queue/polynomial_add.c
@@ -39,6 +39,26 @@ void print_polynomial(Polynomial poly)
     }
 }
 
+void sort_terms(Polynomial *poly)
+{
+    int swapped;
+    Term temp;
+    do
+    {
+        swapped = 0;
+        for (int i = 0; i < poly->num_of_terms - 1; i++)
+        {
+            if (poly->terms[i].exp < poly->terms[i + 1].exp)
+            {
+                temp = poly->terms[i];
+                poly->terms[i] = poly->terms[i + 1];
+                poly->terms[i + 1] = temp;
+                swapped = 1;
+            }
+        }
+    } while (swapped);
+}
+
 // Term get_term(int num)
 // {
 //     Term term;
@@ -55,19 +75,85 @@ void print_polynomial(Polynomial poly)
 
 int main()
 {
-    Polynomial poly_1, poly_2;
+    Polynomial poly_1, poly_2, res;
     printf("Enter details of first polynomial: \n");
     printf("Enter the number of terms: ");
     scanf("%d", &poly_1.num_of_terms);
     get_polynomial(&poly_1);
     printf("You have entered: \n");
+    sort_terms(&poly_1);
     print_polynomial(poly_1);
+
     printf("Enter details of second polynomial: \n");
     printf("Enter the number of terms: ");
     scanf("%d", &poly_2.num_of_terms);
     get_polynomial(&poly_2);
     printf("You have entered: \n");
+    sort_terms(&poly_2);
     print_polynomial(poly_2);
+
+    int i = 0, j = 0, k = -1;
+    while (i < poly_1.num_of_terms && j < poly_2.num_of_terms)
+    {
+        if (poly_1.terms[i].exp == poly_2.terms[i].exp)
+        {
+            k++;
+            res.terms[k].coeff = poly_1.terms[i].coeff + poly_2.terms[j].coeff;
+            res.terms[k].exp = poly_1.terms[i].exp;
+            i++, j++;
+        }
+        // else if (mat_a[i][0] == mat_b[j][0])
+        // {
+        //     if (mat_a[i][1] < mat_b[j][1])
+        //     {
+        //         k++;
+        //         sparce_add(res, mat_a, k, i);
+        //         i++;
+        //     }
+        //     else
+        //     {
+        //         k++;
+        //         sparce_add(res, mat_b, k, j);
+        //         j++;
+        //     }
+        // }
+        else if (poly_1.terms[i].exp > poly_2.terms[i].exp)
+        {
+            k++;
+            res.terms[k].coeff = poly_1.terms[i].coeff;
+            res.terms[k].exp = poly_1.terms[i].exp;
+            i++;
+        }
+        else
+        {
+            k++;
+            res.terms[k].coeff = poly_2.terms[j].coeff;
+            res.terms[k].exp = poly_2.terms[j].exp;
+            j++;
+        }
+    }
+
+    while (i <= poly_1.num_of_terms)
+    {
+        k++;
+        res.terms[k].coeff = poly_1.terms[i].coeff;
+        res.terms[k].exp = poly_1.terms[i].exp;
+        i++;
+    }
+
+    while (j <= poly_2.num_of_terms)
+    {
+        k++;
+        res.terms[k].coeff = poly_2.terms[j].coeff;
+        res.terms[k].exp = poly_1.terms[j].exp;
+        j++;
+    }
+
+    res.num_of_terms = k;
+
+    printf("The sum is :\n");
+    print_polynomial(res);
+    return 0;
 
     // for (int i = 0; i < num; i++)
     // {
